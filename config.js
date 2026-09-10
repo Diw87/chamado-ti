61add7f1dd699bdc1cee869ed4b5e153f254549e
Diw87 <wherbethjk@hotmail.com>
2026-09-10 15:48:38 -0300

    chore: configuração pública do Supabase para v3

---
+++ config.js
@@ -0,0 +1,7 @@
+// CHAMADO T.I. 3.0
+// Preencha SOMENTE com a URL pública do projeto e a chave anon/publishable.
+// Nunca coloque a service_role key neste arquivo ou no GitHub.
+window.CHAMADO_TI_CONFIG = {
+  SUPABASE_URL: '',
+  SUPABASE_ANON_KEY: ''
+};
