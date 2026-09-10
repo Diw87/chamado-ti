a03bedd9dbbd4388a2d7328728d2a00cfb62e354
Diw87 <wherbethjk@hotmail.com>
2026-09-10 17:41:54 -0300

    Carrega correção definitiva do menu após autenticação

--- app.js
+++ app.js
@@ -7,4 +7,5 @@ document.write('<script src="app-utils.js"><\/script>');
 document.write('<script src="username-auth.js"><\/script>');
 document.write('<script src="admin-user-actions.js"><\/script>');
 document.write('<script src="inventory.js"><\/script>');
-document.write('<script src="ui-shell.js?v=20260910-2"><\/script>');
+document.write('<script src="ui-shell.js?v=20260910-3"><\/script>');
+document.write('<script src="auth-shell-fix.js?v=20260910-1"><\/script>');
