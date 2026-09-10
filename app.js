ac9e73b30ff8b02684a871154301b11bef1f6e2b
Diw87 <wherbethjk@hotmail.com>
2026-09-10 16:07:06 -0300

    Carrega módulo de autenticação por usuário

--- app.js
+++ app.js
@@ -4,3 +4,4 @@ document.write('<script src="app-core.js"><\/script>');
 document.write('<script src="app-render.js"><\/script>');
 document.write('<script src="app-actions.js"><\/script>');
 document.write('<script src="app-utils.js"><\/script>');
+document.write('<script src="username-auth.js"><\/script>');
